1786c5e14ae6d4e2e7243c7805f16f1f91e96f83
k3vn6077 <kevinkim749@gmail.com>
2022-12-05 09:03:41 -0500

    Initial Commit

---
+++ appdemo2/appdemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  appdemo2App.swift
+//  appdemo2
+//
+//  Created by Kevin Kim on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct appdemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
